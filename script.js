7ff6a3346cc902d580d35a1b1457a430f8e90dee
Grisha <pin.pinigin@mail.ru>
2023-11-13 21:30:38 +0300

    refactor: commit 2

--- script.js
+++ script.js
@@ -1,56 +1,79 @@
-const API_KEY = 'Jk_KiJlp6ja_M_VKk68esIn4TaWHJ7NxgIm2skBP6_4';
+const API_KEY = 'hpl1Lo9CSi6La1Lu3mL8D6mcyVZaEkWDKFKYZ6YbEEE';
 const url = `https://api.unsplash.com/photos/random?client_id=${API_KEY}&per_page=1`;
 const app = document.getElementById("app");
 
 let storage = sessionStorage.getItem('data');
 
-async function loadData() {
-  try {
+if (!storage) {
+  loadData();
+}
+
+const setDataSessinStore = (data) => {
+  sessionStorage.setItem('data', JSON.stringify(data));
+}
+
+async function loadData(){
+  try{
     const response = await fetch(url);
     const data = await response.json();
-    sessionStorage.setItem('data', JSON.stringify(data));
+
+    setDataSessinStore(data);
     storage = sessionStorage.getItem('data');
     renderContent();
 
   } catch (error) {
     console.error(error);
   }
-}
+};
 
-if (!storage) {
-  loadData();
-}
+const getDataStorege = () => {
+  const data =  JSON.parse(storage);
 
-const renderContent = () => {
-  const data = JSON.parse(storage);
-  if (data !== null) {
-    const content = `
-      <div class="photo" id="${data.id}">
-        <img class="photo__img" src="${data.urls.small}" alt="${data.user.name}">
-        <h3>${data.user.name}</h3>
-        <div class="photo__like">
-          <span id="counter">${data.likes}</span>
-          <button id="btn">Like</button>
+  const id = data.id;
+  const src = data.urls.small;
+  const author = data.user.name;
+  const likes = data.likes;
+
+  return {data, id, src, author, likes};
+};
+
+const addLike = (data) => {
+  const btn = document.getElementById('btn');
+
+  btn.addEventListener('click', () => {  
+    const counter = document.getElementById('counter');
+    data.likes += 1;
+    counter.textContent = data.likes;
+    sessionStorage.setItem('data', JSON.stringify(data));
+  });
+};
+
+const history = (data) => {
+  const history = localStorage.getItem('history');
+  let historyData = history ? JSON.parse(history) : [];
+  historyData.unshift(data);
+  localStorage.setItem('history', JSON.stringify(historyData));
+};
+
+function renderContent(){
+  const {data, id, src, author, likes} = getDataStorege();
+
+  const content = `
+        <div class="photo" id="${id}">
+          <img class="photo__img" src="${src}" alt="${author}">
+          <h3>${author}</h3>
+          <div class="photo__like">
+            <span id="counter">${likes}</span>
+            <button id="btn">Like</button>
+          </div>
         </div>
-      </div>
-    `;
+      `;
 
-    app.innerHTML = content;
+  app.innerHTML = content;
 
-    const btn = document.getElementById('btn');
-    btn.addEventListener('click', () => {
-      const counter = document.getElementById('counter');
-      data.likes += 1;
-      counter.textContent = data.likes;
-      sessionStorage.setItem('data', JSON.stringify(data));
-    });
-
-    const history = localStorage.getItem('history');
-    let historyData = history ? JSON.parse(history) : [];
-    historyData.unshift(data);
-    localStorage.setItem('history', JSON.stringify(historyData));
-  }
-}
+  addLike(data);
+  history(data);
+};
 
 window.addEventListener('load',  () => {
   renderContent();
